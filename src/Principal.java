4be33d5de33b0d1ab09280ec66ca3026d33a329b
lecocchi <leju1712@gmail.com>
2021-08-30 12:12:33 -0300

    Ssave Principal

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,51 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Persona leandro = new Persona();
+
+		leandro.nombre = "Leandro";
+		leandro.apellido = "Cocchi";
+		leandro.edad = 41;
+
+		leandro.caminar();
+		leandro.saltar();
+		leandro.hablar();
+
+		Ascensor ascensor = new Ascensor();
+
+		ascensor.marca = "Hp";
+		ascensor.numeroSerie = 123456;
+
+		ascensor.subir();
+		ascensor.bajar();
+
+		Avion boing = new Avion();
+
+		boing.modelo = "Boing";
+		boing.capacidadPersona = 200;
+		boing.alturaMaxima = 10000;
+
+		boing.volar();
+		boing.aterrizar();
+
+		Deportista messi = new Deportista();
+		messi.nombre = "Leonel";
+		messi.apellido = "Messi";
+		messi.deportePractica = "Futbol";
+
+		messi.correr();
+		messi.descansar();
+		messi.saltar();
+
+		CajeroAutomatico banelco = new CajeroAutomatico();
+		banelco.alto = 10;
+		banelco.color = "Gris";
+		banelco.marca = "Cualquiera";
+
+		banelco.cambiarClave();
+		banelco.depositarCheque();
+		banelco.retirarDinero();
 
 	}
 
